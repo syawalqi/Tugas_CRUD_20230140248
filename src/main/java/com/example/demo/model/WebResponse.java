0a2c63b20c4c01b7d720b54ba4fb2df052529a60
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-03-13 21:52:23 +0700

    feat: add model package with WebResponse for API consistency

---
+++ src/main/java/com/example/demo/model/WebResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private Integer code;
+    private String status;
+    private T data;
+    private String message;
+}
